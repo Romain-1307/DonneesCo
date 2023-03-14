145e8d40ff0149ead8ea25f1f754fed759cac5bf
Romain Poudevigne <poudvmhsc34@gmail.com>
2023-03-14 09:41:36 +0100

    maj

--- index.js
+++ index.js
@@ -6,21 +6,26 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+// var cors = require('cors');
+
 var data=[];
 
 app.use(express.static('html'));
 
-app.post("/annotation", cors(), function(req, res){
-    var body = req.body;
-    data.push(body);
-    res.send("Votre commentaire a été sauvegardé !");
+// app.post("/annotation", cors(), function(req, res){
+	
+app.post("/annotation", function(req, res){
+	var body = req.body;
+	// console.log(body);
+	data.push(body);
+	// console.log(data);
+	res.send("votre commentaire a été sauvegardé !");
 });
 
-/*app.get("/toto", function(req, res){
-    res.send("salut toto !");
-});*/
+// app.get("/IdURI", function(req, res){
+	// res.send("salut toto!");
+// });
 
 app.listen(port, function(){
-    console.log('serveur listening on port : '+port);
+	console.log('serveur listening on port : '+port);
 });
-
